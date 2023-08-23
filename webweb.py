2679555be2b114f2ceb90debdd7ecf4db15ff772
Noah Barton <107448859+bartonnoah@users.noreply.github.com>
2023-08-22 23:01:49 -0700

    added psuedocode

--- webweb.py
+++ webweb.py
@@ -1,7 +1,12 @@
 import speedtest
 from ping3 import ping, verbose_ping
+from datetime import datetime
+import pandas as pd
 
 target_host = "google.com"
+spectrum_dl = 500 #(Mbps), quoted avlue by Spectrum
+acceptable_ping = 69 #(ms), acceptable number we need for gaming, ask Michael & Cass
+acceptable_packet_loss = 69 #(%), acceptable number we need for gaming, ask Michael & Cass
 
 def get_speeds():
     st = speedtest.Speedtest()
@@ -14,9 +19,23 @@ def get_ping():
     return response_time
 
 def get_packet_loss():
-    packet_loss = ping(target_host, count=10)  # Send 10 ping requests
+    packet_loss = ping(target_host, count=10)
     return packet_loss
 
-print(get_speeds())
-print(get_ping())
-print(get_packet_loss())
+def alarms(speeds, ping_speed, packet_loss):
+    if speeds[0] < 500:
+        #Do something
+    if ping_speed > acceptable_ping:
+        #Do something else
+    if packet_loss > acceptable_packet_loss:
+        #Do a third thing
+
+def save_stats(speeds, ping, packet_loss):
+    #Save stats to a daily CSV
+    return 'TODO Implement'
+
+speeds = get_speeds()
+ping_speed = get_ping()
+packet_loss = get_packet_loss()
+
+save_stats(speeds, ping_speed, packet_loss)
